27ed56eb825256dc8aac8e9950cc1acccace6ced
Chayya_Syal <chayya.syal@bbc.cuk>
2017-11-28 14:20:36 +0000

    Added an item_spec test which contains descriptions of its properties. All 4 spec tests are currently passing.

---
+++ spec/item_spec.rb
@@ -0,0 +1,16 @@
+describe Item do
+  describe '#name' do
+    it 'has the name of item being sold' do
+    end
+  end
+
+  describe '#sell_in' do
+    it 'tells us how many days are left until item is sold' do
+    end
+  end
+
+  describe '#quality' do
+    it 'tells us how valuable an item is' do
+    end
+  end
+end
